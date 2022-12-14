d3092636e67f086b4ee7af493de58bafbc0039a6
nmofficial <nmv.co.uk@gmail.com>
2022-12-14 04:57:19 -0500

    small changes to debug, not functional

--- src/Thompson.hs
+++ src/Thompson.hs
@@ -1,13 +1,11 @@
 module Thompson (showParser) where
 
-import Text.ParserCombinators.Parsec
+import Text.ParserCombinators.Parsec ( (<?>), (<|>), Parser, parse )
 import qualified Text.ParserCombinators.Parsec.Token as P
-import Text.ParserCombinators.Parsec.Language
-import Data.List ( intercalate )
-import GHC.RTS.Flags (ProfFlags(doHeapProfile))
+import Text.ParserCombinators.Parsec.Language ( emptyDef )
 import Data.Char (ord)
 
-data Node = Concat Node Node | Star Node | Or Node Node | Character Int
+data Node = Concat Node Node | Star Node | Or Node Node | Character Int deriving (Show)
 
 showParser :: Parser Node 
 showParser =
@@ -32,8 +30,9 @@ kleeneParser = do
 
 concatParser :: Parser Node
 concatParser = do
-	left <- showParser
-	right <- charParser
+	left <- charParser
+	symbol "."
+	right <- showParser
 	return (Concat left right)
 
 charParser :: Parser Node
