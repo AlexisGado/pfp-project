2217046bd60febfd8893da2783bf1a2f921cbf32
AlexisGado <ipod.alexisgado@gmail.com>
2022-12-16 17:53:41 -0500

    formatting + removing old parser

--- src/Thompson.hs
+++ src/Thompson.hs
@@ -1,52 +1,64 @@
-module Thompson (showParser) where
+module Thompson (regexParser, makeNFA) where
+import           Automaton                          (Automaton (..), Label (..))
+import           Control.Monad                      (msum)
+import           Data.Char                          (ord)
+import qualified Data.Map                           as Map
+import qualified Data.Set                           as Set
+import qualified Text.ParserCombinators.Parsec      as P
+import qualified Text.ParserCombinators.Parsec.Expr as PE
 
-import Text.ParserCombinators.Parsec ( (<?>), (<|>), Parser, parse )
-import qualified Text.ParserCombinators.Parsec.Token as P
-import Text.ParserCombinators.Parsec.Language ( emptyDef )
-import Data.Char (ord)
+data Node = Concat !Node !Node | Star !Node | Or !Node !Node | Character !Int deriving(Show)
 
-data Node = Concat Node Node | Star Node | Or Node Node | Character Int deriving (Show)
-
-showParser :: Parser Node 
-showParser =
-	parenParser <|> -- ( ... )
-	kleeneParser <|> -- a*
-    charParser <|>    -- single char
-	concatParser <|> -- ab
-	orParser <?> "Parse error"
-
-orParser :: Parser Node
-orParser = do
-	left <- showParser
-	symbol "|"
-	right <- showParser
-	return (Or left right)
-
-kleeneParser :: Parser Node
-kleeneParser = do
-	left <- showParser
-	symbol "*"
-	return (Star left)
-
-concatParser :: Parser Node
-concatParser = do
-	left <- charParser
-	symbol "."
-	right <- showParser
-	return (Concat left right)
-
-charParser :: Parser Node
-charParser = do
-	left <- character
-	return (Character (ord left))
-
-parenParser :: Parser Node
-parenParser = do
-	ls <- parens showParser
-	return ls
+regexParser :: P.Parser Node
+regexParser = PE.buildExpressionParser opTable base
+  where
+    opTable = [   [ PE.Postfix (P.char '*' >> return Star)]
+                , [ PE.Infix (return Concat) PE.AssocLeft]
+                , [ PE.Infix (P.char '|' >> return Or) PE.AssocLeft]
+              ]
+    base = msum [Character . ord <$> P.noneOf "()*|", parens regexParser]
+    parens = P.between (P.char '(') (P.char ')')
 
 
-lexer = P.makeTokenParser emptyDef
-parens = P.parens lexer
-character = P.charLiteral lexer
-symbol = P.symbol lexer
+
+
+
+makeNFA :: Either a Node -> Automaton
+makeNFA ((Right ast)) = Automaton table alphabet start end where
+  (table, _) = thompsons ast 1 0 1
+  alphabet = Set.toList (buildAlph ast Set.empty)
+  start = Set.singleton 1
+  end = Set.singleton 0
+makeNFA (Left _) = error "Bad AST"
+
+
+
+buildAlph :: Node -> Set.Set Label -> Set.Set Label
+buildAlph (Concat r l) alph  = Set.union (buildAlph r alph) (buildAlph l alph)
+buildAlph (Star l) alph      = buildAlph l alph
+buildAlph (Or r l) alph      = Set.union (buildAlph r alph) (buildAlph l alph)
+buildAlph (Character x) alph = Set.insert (Label x) alph
+
+
+thompsons :: (Num b, Ord b) => Node -> b -> b -> b -> (Map.Map b [(Label, b)], b)
+thompsons (Character x) q f l = (Map.fromList [(q, [(Label x, f)])], l)
+thompsons (Concat s t) q f l = (Map.union smap tmap, lt) where
+  (smap, ls) = thompsons s q i ln
+  (tmap, lt) = thompsons t i f ls
+  i = l + 1
+  ln = l + 1
+thompsons (Or s t) q f l = (Map.union (Map.union smap tmap) omap, lt) where
+  omap = Map.fromList [(q, [(Epsilon, si), (Epsilon, ti)]), (sf, [(Epsilon, f)]), (tf, [(Epsilon, f)])]
+  (smap, ls) = thompsons s si sf ln
+  (tmap, lt) = thompsons t ti tf ls
+  si = l+1; sf = l+2
+  ti = l+3; tf = l+4
+  ln = l+4
+thompsons (Star s) q f l = (Map.union smap stmap, ls) where
+  stmap = Map.fromList [(q, [(Epsilon, si), (Epsilon, f)]), (sf, [(Epsilon, si), (Epsilon, f)])]
+  (smap, ls) = thompsons s si sf ln
+  si = l+1; sf = l+2
+  ln = l+2
+
+--showNode :: Either a Node -> String
+--showNode (Right (Concat x y)) = show "hi"
